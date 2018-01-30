db1facf784fadcf1429fe29692ca6c932d12fd66
Chaitanya <chaitanyapandit@me.com>
2018-01-30 16:01:11 +0530

    typecasting to NSString

--- Pod/Classes/Model/Settings.swift
+++ Pod/Classes/Model/Settings.swift
@@ -36,9 +36,9 @@ final class Settings : BSImagePickerSettings {
         paragraphStyle.lineBreakMode = .byTruncatingTail
         paragraphStyle.alignment = .center
         return [
-            NSFontAttributeName: UIFont.boldSystemFont(ofSize: 10.0),
-            NSParagraphStyleAttributeName: paragraphStyle,
-            NSForegroundColorAttributeName: UIColor.white
+            NSFontAttributeName as NSString: UIFont.boldSystemFont(ofSize: 10.0),
+            NSParagraphStyleAttributeName as NSString: paragraphStyle,
+            NSForegroundColorAttributeName as NSString: UIColor.white
         ]
     }()
     var backgroundColor: UIColor = UIColor.white
